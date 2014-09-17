1d63fe0e760bc1c746c78baa221aabefde924f29
Shane McCulley <smcculle@uno.edu>
2014-09-17 13:27:09 -0500

    new TestGameInterface.java

---
+++ TestGameInterface.java
@@ -0,0 +1,15 @@
+/**
+ * Risk Game Test Harness
+ * CSCI 2120 Fall 2014
+ * @author Shane McCulley
+ * @date September 17, 2014
+ * @version 0.1
+ **/
+ 
+ import junit.framework.TestCase;
+ 
+ public class TestGameInterface extends TestCase {
+ 
+ 
+ 
+ }
