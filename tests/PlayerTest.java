4fd6fa54319c34f1c888353ae9c6a689908c67b5
Shane McCulley <smcculle@uno.edu>
2014-09-17 13:25:22 -0500

    PlayerTest skeleton

---
+++ tests/PlayerTest.java
@@ -0,0 +1,77 @@
+/**
+ * Risk Game Test Harness
+ * CSCI 2120 Fall 2014
+ * @author Shane McCulley
+ * @date September 17, 2014
+ * @version 0.1
+ **/
+ 
+ import junit.framework.TestCase;
+ 
+ public class PlayerTest extends TestCase() {
+     
+     //instance variables 
+     
+     protected setUp() {
+         //instantiate them
+     }
+     
+     // Testing Player's String getName() method
+     public void testGetName() {
+         
+     }
+     
+     // Testing Player's int getUnplacedArmies() method
+    public void testGetUnplacedArmies() {
+        
+    }
+     
+    // Testing Player's int getTotalArmies() method
+    public void testGetTotalArmies() {
+        
+    } 
+    
+    // Testing Player's ArrayList<Territory> getTerritoriesList() method
+    public void testGetTerritoriesList() {
+        
+    }
+     
+    // Testing Player's ArrayList<Continent> getContinentsList() method
+    public void testGetContinentsList() {
+        
+    }
+    
+
+    // Testing Player's void addTerritory(Territory) method
+    public void testAddTerritory() {
+        
+    }
+    
+    // Testing Player's void addContinent(Continent) method
+    public void testAddContinent() {
+        
+    }
+    
+    // Testing Player's void addArmies(int) method
+    public void testAddArmies() {
+        
+    }
+    
+    // Testing Player's void addCard(Card) method
+    public void testAddCard() {
+        
+    }
+    
+    // Testing Player's void addTerritory() method
+    public void addTerritory() {
+        
+    }
+     
+    /*Testing Player's void attack method
+     *void attack(Territory attacker, Territory defender, int numAttackingArmies)
+    */
+    public void testAttack() {
+        
+    } 
+ }
+ 
